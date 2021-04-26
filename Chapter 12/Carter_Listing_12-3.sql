226e6d28c74cabb412484d79807b9f50e16d3719
lorran <lorranpimenta@gmail.com>
2021-04-26 17:24:49 -0300

    Inclusao Backup 12-3

--- Chapter 12/Carter_Listing_12-3.sql
+++ Chapter 12/Carter_Listing_12-3.sql
@@ -2,3 +2,13 @@ EXEC sp_addumpdevice  @devtype = 'disk',
                       @logicalname = 'Chapter12Backup', 
                       @physicalname = 'C:\MSSQL\Backup\Chapter12Backup.bak' ;
 GO
+
+
+BACKUP DATABASE Chapter12
+        TO  [Chapter12Backup]
+        WITH  RETAINDAYS = 90
+        , FORMAT
+        , INIT
+        , MEDIANAME = 'Chapter12'
+        , NAME = 'Chapter12-Full Database Backup'
+        , COMPRESSION ;
